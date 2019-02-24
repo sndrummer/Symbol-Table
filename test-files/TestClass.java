7bdf64395e286f39457a2a3c9a1c2fec1fc66d85
Samuel Nuttall <sndrummer@gmail.com>
2019-02-24 00:39:33 -0700

    changed .gitignore

---
+++ test-files/TestClass.java
@@ -0,0 +1,19 @@
+package edu.byu.yc;
+
+public class TestClass {
+
+
+    private int cheese = 4;
+
+    public String getCheese() {
+        return Integer.toString(cheese);
+    }
+
+    public int hereYouGo() {
+        return cheese;
+    }
+
+    public void what(String h, int l) {
+        System.out.println(h);
+    }
+}
