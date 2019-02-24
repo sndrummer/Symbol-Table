7cd4d0db3e580744ee172820ae63e137c695d7e0
Samuel Nuttall <sndrummer@gmail.com>
2019-02-24 01:01:53 -0700

    almost done next step testing

--- src/main/java/edu/byu/yc/symboltable/ClassFieldsMethodsParams.java
+++ src/main/java/edu/byu/yc/symboltable/ClassFieldsMethodsParams.java
@@ -1,19 +1,15 @@
 package edu.byu.yc.symboltable;
 
-
-import org.eclipse.jdt.core.dom.AST;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import edu.byu.yc.symboltable.ASTNameType;
 
 /**
  * @author Samuel Nuttall
  *
- * ClassFieldsMethodsParams contains an individual class' fields, methods, and params types and names
+ * ClassFieldsMethodsParams contains an individual class's fields, methods, and params types and names
  * This class is an integral part of the symbol table and holds all the relevent information for an
  * a class that has been visited by the symbol table visitor.
  */
@@ -24,6 +20,11 @@ public class ClassFieldsMethodsParams {
     private List<ASTNameType> fields;
     private Map<ASTNameType, List<ASTNameType>> methodParamsMap;
 
+    /**
+     * Constructor for the class fields, methods, and parameters
+     *
+     * @param classFQN the fully qualified class name of the class
+     */
     public ClassFieldsMethodsParams(String classFQN) {
         this.classFQN = classFQN;
         fields = new ArrayList<>();
@@ -32,24 +33,44 @@ public class ClassFieldsMethodsParams {
 
     }
 
+    /**
+     * Returns the field type that matches the given field name
+     *
+     * @param fieldName String Name of the field
+     * @return String representation of the type of the field that matches the fieldName param
+     */
     public String getFieldTypeByName(String fieldName) {
-        String type = null;
+        String fieldType = null;
         for (ASTNameType nameType : fields) {
             if (nameType.getName().equals(fieldName))
-                type = nameType.getType();
+                fieldType = nameType.getType();
         }
-        return type;
+        return fieldType;
     }
 
+    /**
+     * Returns the method type that matches the given method name
+     *
+     * @param methodName String name of the method
+     * @return String representation of the return type of the method
+     */
     public String getMethodTypeByName(String methodName) {
-        String type = null;
+        String methodType = null;
         for (Map.Entry<ASTNameType, List<ASTNameType>> entry: methodParamsMap.entrySet()) {
             if (entry.getKey().getName().equals(methodName))
-                type = entry.getKey().getType();
+                methodType = entry.getKey().getType();
         }
-        return type;
+        return methodType;
     }
 
+
+    /**
+     * Returns the type of the parameter that matches the given method name and parameter name
+     *
+     * @param methodName String name of the method
+     * @param paramName String name of the parameter
+     * @return String representation of the type of the parameter
+     */
     public String getParamTypeByName(String methodName, String paramName) {
        String type = "";
         List<ASTNameType> params = methodParamsMap.get(methodName);
@@ -84,9 +105,9 @@ public class ClassFieldsMethodsParams {
     @Override
     public String toString() {
         return "ClassFieldsMethodsParams{" +
-                "classFQN='" + classFQN + '\'' +
-                ", fields=" + fields +
-                ", methodParamsMap=" + methodParamsMap +
+                "Class FQN ='" + classFQN + '\'' +
+                ", Fields =" + fields +
+                ", Method-Params =" + methodParamsMap +
                 '}';
     }
 }
